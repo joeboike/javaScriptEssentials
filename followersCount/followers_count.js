7cb44f350c9ac8b2501757d459f5c6e4e445a2e7
joeboike <joeboike@gmail.com>
2026-09-14 09:00:16 -0400

    added reset

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -1,4 +1,5 @@
 let count = 0; // Initialize count to 0
+let prev_count=0;
 
 function increaseCount() {
   count++; // Increment the count by 1
@@ -14,10 +15,19 @@ function increaseCount() {
   checkCountValue(); // Check count value and display messages
 }
 
-function checkCountValue() {
+function resetCount() {
+    prev_count = count;
+    count = 0; // Increment the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+  }
+  
+  function checkCountValue() {
   if (count === 10) {
     alert("Your Instagram post gained 10 followers! Congratulations!");
   } else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");
+  } else if (count === 0 && prev_count !== 0) {
+    alert("Your Instagram followers count was reset!")
   }
 }
